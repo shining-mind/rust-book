f2a77725ad02d06d1f475c21017f314ae0cfbe39
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2024-04-03 13:50:58 +0700

    chore(guessing-game): revisit and refactor

--- chapter-02/guessing-game/src/main.rs
+++ chapter-02/guessing-game/src/main.rs
@@ -1,35 +1,40 @@
-use std::io;
-use std::cmp::Ordering;
+//! This is a classic guessing game. The game will generate random number between 1 and 100.
+//! The user must guess the number.
+
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
+    let secret = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1..=100); 
 
     loop {
-        println!("Please input your guess:");
+        let guess = ask();
 
-        let mut guess = String::new();
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(value) => value,
-            Err(_) => continue,
-        };
-    
-        println!("You guessed: {guess}");
-    
-        match guess.cmp(&secret_number) {
+        match guess.cmp(&secret) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too large"),
             Ordering::Equal => {
-                println!("You win");
+                println!("You guessed correctly!");
                 break;
             },
         }
     }
+}
+
+/// Ask the user to guess the number and return it
+fn ask() -> u32 {
+    println!("Please enter your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    return match guess.trim().parse::<u32>() {
+        Ok(value) => value,
+        Err(_) => ask()
+    };
 }
